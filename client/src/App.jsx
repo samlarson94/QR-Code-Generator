44de18ddd9f4b16303ba8dabd3a3d3e448db74dc
samlarson94 <samlarson94@gmail.com>
2022-07-11 18:51:11 -0500

    Adding input and button to App.jsx + CSS styling.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Hello World!</h1>
+      <h1>QR Code Generator</h1>
+      <input type="text" placeholder="https://samlarson.tech" />
+      <button class="generate-btn">Generate QR Code</button>
      
     </div>
   )
