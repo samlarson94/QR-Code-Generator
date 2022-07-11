d117bcf055eb09b559ad681c9befaf9488ec0f8f
samlarson94 <samlarson94@gmail.com>
2022-07-11 18:57:41 -0500

    Adding image to App.jsx, additional styling for button, links, and image.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
+import QRCode from 'qrcode';
+import { useState } from 'react';
 
 
 function App() {
+  const [url, setUrl] = useState('')
+  const [qrcode, setQrcode] = useState('')
 
   return (
     <div className="app">
       <h1>QR Code Generator</h1>
       <input type="text" placeholder="https://samlarson.tech" />
       <button class="generate-btn">Generate QR Code</button>
-     
+      <img src="" alt="" />
     </div>
   )
 }
