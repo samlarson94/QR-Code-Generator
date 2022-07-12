37f784acbfd117a371976bd07082976b1cd0e695
samlarson94 <samlarson94@gmail.com>
2022-07-11 19:03:36 -0500

    Adding an onClick Event Listener to Generate Button - tying to GenerateQRCode function.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     <div className="app">
       <h1>QR Code Generator</h1>
       <input type="text" placeholder="https://samlarson.tech" />
-      <button class="generate-btn">Generate QR Code</button>
+      <button onClick={GenerateQRCode} class="generate-btn">Generate QR Code</button>
       <img src={qrcode} />
     </div>
   )
