6917dfbaad3bb58501f130366bb567e97399b0c6
samlarson94 <samlarson94@gmail.com>
2022-07-11 19:02:19 -0500

    Importing QRCode and useState to App.jsx.  Adding GenerateQRCode function.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,28 @@ import { useState } from 'react';
 
 
 function App() {
+  //Set up Use States
   const [url, setUrl] = useState('')
   const [qrcode, setQrcode] = useState('')
+
+  //Generate QR Code function - Image only needs a QR Code to display
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(url, (err, url) => {
+      if (err) {
+        return console.error(err);
+      }else {
+        console.log(url);
+        setQrcode(url);
+      }
+    })
+  }
 
   return (
     <div className="app">
       <h1>QR Code Generator</h1>
       <input type="text" placeholder="https://samlarson.tech" />
       <button class="generate-btn">Generate QR Code</button>
-      <img src="" alt="" />
+      <img src={qrcode} />
     </div>
   )
 }
