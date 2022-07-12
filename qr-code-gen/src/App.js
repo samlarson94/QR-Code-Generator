17067c55a3800e799447ccb1ed4984bc79a69eac
samlarson94 <samlarson94@gmail.com>
2022-07-11 19:38:36 -0500

    Adding correct code to App.js and index.css within qr-code-gen.

--- qr-code-gen/src/App.js
+++ qr-code-gen/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import QRCode from 'qrcode';
+import { useState } from 'react';
+
 
 function App() {
+  //Set up Use States
+  const [url, setUrl] = useState('')
+  const [qrcode, setQrcode] = useState('')
+
+  //Generate QR Code function - Image only needs a QR Code to display
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(url, {
+      width: 800,
+      margin: 2,
+      color: {
+        dark: '#ffffffff',
+        light: '#000000ff'
+      }
+    }, (err, url) => {
+      if (err) {
+        return console.error(err);
+      } else {
+        console.log(url);
+        setQrcode(url);
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>QR Code Generator</h1>
+      <input type="text" placeholder="https://samlarson.tech" value={url}
+      onChange={(evt) => setUrl(evt.target.value)} />
+      <button onClick={GenerateQRCode} class="generate-btn">Generate QR Code</button>
+      {/* Wrapping QRCode in Conditional - Adding link to Download */}
+      {qrcode && <>
+        <img src={qrcode} />
+        <a href={qrcode} download="qrcode.png">Download</a>
+        </>
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
